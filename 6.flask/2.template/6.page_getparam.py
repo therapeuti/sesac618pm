06bd8f4cd7df797d069ba16eec1a38cc12933e5f
therapeuti <lorraine.dubled@gmail.com>
2025-07-08 00:07:49 +0900

    get parameter로 페이징

--- 6.flask/2.template/6.page_getparam.py
+++ 6.flask/2.template/6.page_getparam.py
@@ -4,13 +4,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # 더미 유저 100명 생성
-users = [{'id': i, 'name': f'user{i}', 'age': 20+i % 10, 'mobile':f'010-0000-{str(i).zfill(4)}'} for i in range(1, 100)]
+users = [{'id': i, 'name': f'user{i}', 'age': 20+i % 10, 'mobile':f'010-0000-{str(i).zfill(4)}'} for i in range(1, 101)]
 
 
-# http://localhost:5000/?pages=1
+# http://localhost:5000/?page=1
 @app.route('/')
 def index():
-    page = int(request.args.get('page'))
+    page = request.args.get('page', default=None, type=int)
+    print(page)
     return render_template('users.html', users=users, page=page)
 
 if __name__=='__main__':
