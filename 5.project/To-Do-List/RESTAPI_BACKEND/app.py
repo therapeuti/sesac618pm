f10345d2734fd8afc85b44370261d93ea91b1986
therapeuti <lorraine.dubled@gmail.com>
2025-07-10 17:36:15 +0900

    투두리스트 서버사이드렌더링

---
+++ 5.project/To-Do-List/RESTAPI_BACKEND/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file('todolist.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
