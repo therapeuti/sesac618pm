112cf585a90a63f22547a7354f1a5ec9d0a4038f
therapeuti <lorraine.dubled@gmail.com>
2025-07-18 00:15:06 +0900

    users 검색, 정렬기능 추가

--- 5.project/CRM/app.py
+++ 5.project/CRM/app.py
@@ -18,11 +18,31 @@ count_per_page = 10
 @app.route('/api/getUsers/')
 def send_users():
     page = request.args.get('page', default=1, type=int)
-    logging.debug(f'page 겟 파라미터 : {page}')
-    users = get_users_list(count_per_page, page)
-    all_users = count_users()
-    total_pages = math.ceil(all_users / count_per_page)
-    logging.debug(f'send_user() : {users[0]['id'], users[0]['name']}')
+    u_id = request.args.get('id', type=str)
+    name = request.args.get('name', type=str)
+    address = request.args.get('address', type=str)
+    gender = request.args.get('gender', type=str)
+    orderby = request.args.get('orderby', default='name', type=str)
+    logging.debug(f'GET 파라미터 : {page}, {u_id}, {name}, {address}, {gender}, {orderby}')
+    
+    filtering = {}
+    filtering['page'] = page
+    filtering['orderby'] = orderby
+    if u_id:
+        filtering['id'] = u_id
+    if name:
+        filtering['name'] = name
+    if address:
+        filtering['address'] = address
+    if gender:
+        filtering['gender'] = gender
+
+    logging.debug(filtering)
+
+    users, count_users = get_users_list(count_per_page, filtering)
+      
+    total_pages = math.ceil(count_users / count_per_page)
+    logging.debug(f'send_user() : {users}')
     logging.debug(f'전체 페이지 수: {total_pages}')
     return jsonify({'users':users, 'total_pages':total_pages})
 
