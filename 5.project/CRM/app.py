525556d89c4f44b636f59fbecb500279f531c098
therapeuti <lorraine.dubled@gmail.com>
2025-07-24 14:34:44 +0900

    검색 기능 수정, 로그인페이지 css 화이트톤으로 수정

--- 5.project/CRM/app.py
+++ 5.project/CRM/app.py
@@ -106,4 +106,4 @@ def add_order():
     return jsonify({'order':new_order, 'orderitems':new_orderitems})
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
