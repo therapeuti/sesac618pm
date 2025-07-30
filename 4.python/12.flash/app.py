4a94ff8ab6eb568e1a2fa38bb012c7fef986a58d
therapeuti <lorraine.dubled@gmail.com>
2025-07-30 17:41:00 +0900

    flask flash

--- 4.python/12.flash/app.py
+++ 4.python/12.flash/app.py
@@ -1,8 +1,9 @@
-from flask import Flash, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 
-app = Flash(__name__)
+app = Flask(__name__)
+app.secret_key = 'm'
 
-users = []
+users = [{'id': 'tm', 'pw':'pw', 'name':'태민'}]
 
 @app.route('/')
 def index():
@@ -14,14 +15,17 @@ def login():
     if request.method == 'POST':
         u_id = request.form.get('id')
         pw = request.form.get('pw')
-        user = next((u for u in users if u['id']==id and u['pw']==pw))
+        user = next((u for u in users if u['id']==u_id and u['pw']==pw), None)
         if user:
-            flash('로그인에 성공했습니다.')
             session['user'] = user
             return redirect(url_for('user'))
+        else:
+            flash('로그인 실패')
+            return redirect(url_for('login'))
     else:
         if 'user' in session:
             flash('이미 로그인된 사용자')
+            print(session['user'])
             return redirect(url_for('user'))   
 
     return render_template('login.html')
@@ -31,13 +35,18 @@ def login():
 def user():
     user = session.get('user')
     if user:
+        flash('로그인에 성공했습니다.')
         return render_template('user.html', user=user)
+    flash('비정상 접근. 로그인 먼저 하세요.')
     return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
     session.pop('user', None)
-    flash('정상적으로 로그인 되었씁니다.')
+    flash('정상적으로 로그아웃 되었씁니다.')
     return redirect(url_for('login'))
+
+
+
 if __name__=='__main__':
     app.run(debug=True)
