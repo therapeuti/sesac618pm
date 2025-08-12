795fa21a1395e8472b995334d6f26c28638f2483
therapeuti <lorraine.dubled@gmail.com>
2025-08-12 12:47:22 +0900

    node & react

---
+++ 8.Node/hello.js
@@ -0,0 +1,10 @@
+console.log('hello')
+
+// 노드 개발의 시작은 npm init-y
+
+//react 시작은
+// npx create-react-app sesac  //sesac 폴더에 리액트 구성요소 설치. 이 폴더에서만 리액트 사용 가능
+// 리액트 구성요소들 설치되면 필요없는거 삭제
+// html, css, js... 등 가지고 알아서 만들면 됨.
+// 컴포넌트 하나 씩 반환....
+// 컴포넌트 여러개 반환하려면 div로 묶어서 반환...
